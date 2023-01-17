25f7ff7cc920f3f1cc02c17d897d53cf49f2c174
InhiBiToRs <tanakrit64566@gmail.com>
2023-01-17 16:46:36 +0700

    lab15_1_complete

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,7 +3,20 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	for( i = 0;i < N-1;i++){
+		for(T j=0;j<i+1;j++){
+			if(d[j]<d[i+1]){
+				T y = d[i+1];
+				d[i+1]=d[j];
+				d[j]=y;
+			}
+		}
+	cout << "Pass " << i+1<< ":";
+	for(T i = 0; i < 10; i++){
+		cout << d[i] << " ";
+	}
+	cout<< endl;
+	}
 }
 
 int main(){
